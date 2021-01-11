14b9f8c6dbf2a2c0abc87e78c43f8a4377f2a6a9
Tarun Patel <tarunpatel@1860.gmail.com>
2021-01-12 00:32:35 +0530

    Added haal chaal ask

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
             This is first React App by Tarun Patel !! Hello There !!
+            How are u doin man !
         </p>
       </header>
     </div>
