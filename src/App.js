977309890907fa0d719157e8dcc6aa423e5fb029
Tarun Patel <tarunpatel@1860.gmail.com>
2021-01-12 00:00:07 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            This is first React App by Tarun Patel !! Hello There !!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
